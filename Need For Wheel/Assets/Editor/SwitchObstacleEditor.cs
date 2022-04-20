45dd8ad6de64a6769c21b120ab3c06f77648249c
WashFall <carl.christensen98@gmail.com>
2022-04-20 11:30:10 +0200

    WIP on obstacle dropdown

--- Need For Wheel/Assets/Editor/SwitchObstacleEditor.cs
+++ Need For Wheel/Assets/Editor/SwitchObstacleEditor.cs
@@ -6,35 +6,82 @@ using UnityEditor;
 [CustomEditor(typeof(SwitchObstacleComponent))]
 public class SwitchObstacleEditor : Editor
 {
-	MeshFilter meshfilter;
-	GameObject obstacle;
+    MeshFilter meshfilter;
+    GameObject obstacle;
 
-	public override void OnInspectorGUI()
-	{
-		base.OnInspectorGUI();
+    string searchPath = "Assets/Prefabs/Obstacles";
+    GameObject[] obstaclePrefabs;
+    List<string> obstacleNames = new List<string>();
 
-		SwitchObstacleComponent script = (SwitchObstacleComponent)target;
-		meshfilter = script.GetComponent<MeshFilter>();
+    int selected = 0;
 
-		if (GUILayout.Button("'Box'"))
-		{
-			SetMesh("Cube.fbx");
-		}
-
-		if (GUILayout.Button("'Tree'"))
-		{
-			SetMesh("Cylinder.fbx");
-		}
-
-		if (GUILayout.Button("'Person'"))
-		{
-			SetMesh("Capsule.fbx");
-		}
-	}
-
-	private void SetMesh(string name)
+    private void LoadAssets()
     {
-		Undo.RecordObject(meshfilter, "Name of my operation after this Undo-block");
-		meshfilter.mesh = Resources.GetBuiltinResource<Mesh>(name);
-	}
+        var filePaths = GetAllFilePathsAtFolder($"t:Prefab", searchPath);
+        List<GameObject> files = new List<GameObject>();
+        foreach (var path in filePaths)
+        {
+            GameObject temporary = AssetDatabase.LoadAssetAtPath<GameObject>(path);
+            files.Add(temporary);
+        }
+        obstaclePrefabs = files.ToArray();
+
+        foreach(var obstaclePrefab in obstaclePrefabs)
+        {
+            obstacleNames.Add(obstaclePrefab.name);
+        }
+    }
+
+    private void OnEnable()
+    {
+        //Debug.Log("hej");
+        LoadAssets();
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        SwitchObstacleComponent script = (SwitchObstacleComponent)target;
+        meshfilter = script.GetComponent<MeshFilter>();
+
+        selected = EditorGUILayout.Popup("Obstacle", selected, obstacleNames.ToArray());
+
+
+
+        //if (GUILayout.Button("'Box'"))
+        //{
+        //    SetMesh("Cube.fbx");
+        //}
+
+        //if (GUILayout.Button("'Tree'"))
+        //{
+        //    SetMesh("Cylinder.fbx");
+        //}
+
+        //if (GUILayout.Button("'Person'"))
+        //{
+        //    SetMesh("Capsule.fbx");
+        //}
+    }
+
+    private void SetMesh(string name)
+    {
+        Undo.RecordObject(meshfilter, "Name of my operation after this Undo-block");
+        meshfilter.mesh = Resources.GetBuiltinResource<Mesh>(name);
+    }
+
+    private static string[] GetAllFilePathsAtFolder(string filter, string folderPath)
+    {
+        List<string> filePaths = new List<string>();
+
+        // search for a ScriptObject called CatchphraseCategoryData inside the PhraseDataFolder
+        var guids = AssetDatabase.FindAssets($"{filter}", new string[] { folderPath });
+        foreach (string guid in guids)
+        {
+            string path = AssetDatabase.GUIDToAssetPath(guid);
+            filePaths.Add(path);
+        }
+        return filePaths.ToArray();
+    }
 }
