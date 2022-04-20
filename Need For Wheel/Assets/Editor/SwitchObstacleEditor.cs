49c7c3106e77800660b1768f6ce85c2a997deb54
WashFall <carl.christensen98@gmail.com>
2022-04-20 11:49:38 +0200

    Finished Switch Obstacle Component
    Every prefab added to the Obstacles folder will now be interchangeable in the Obstacle_Holder prefab

--- Need For Wheel/Assets/Editor/SwitchObstacleEditor.cs
+++ Need For Wheel/Assets/Editor/SwitchObstacleEditor.cs
@@ -6,9 +6,6 @@ using UnityEditor;
 [CustomEditor(typeof(SwitchObstacleComponent))]
 public class SwitchObstacleEditor : Editor
 {
-    MeshFilter meshfilter;
-    GameObject obstacle;
-
     string searchPath = "Assets/Prefabs/Obstacles";
     GameObject[] obstaclePrefabs;
     List<string> obstacleNames = new List<string>();
@@ -34,7 +31,6 @@ public class SwitchObstacleEditor : Editor
 
     private void OnEnable()
     {
-        //Debug.Log("hej");
         LoadAssets();
     }
 
@@ -42,33 +38,23 @@ public class SwitchObstacleEditor : Editor
     {
         base.OnInspectorGUI();
 
-        SwitchObstacleComponent script = (SwitchObstacleComponent)target;
-        meshfilter = script.GetComponent<MeshFilter>();
-
+        EditorGUI.BeginChangeCheck();
         selected = EditorGUILayout.Popup("Obstacle", selected, obstacleNames.ToArray());
-
-
-
-        //if (GUILayout.Button("'Box'"))
-        //{
-        //    SetMesh("Cube.fbx");
-        //}
-
-        //if (GUILayout.Button("'Tree'"))
-        //{
-        //    SetMesh("Cylinder.fbx");
-        //}
-
-        //if (GUILayout.Button("'Person'"))
-        //{
-        //    SetMesh("Capsule.fbx");
-        //}
+        if (EditorGUI.EndChangeCheck())
+        {
+            SetPrefab(selected);
+        }
     }
 
-    private void SetMesh(string name)
+    private void SetPrefab(int prefabIndex)
     {
-        Undo.RecordObject(meshfilter, "Name of my operation after this Undo-block");
-        meshfilter.mesh = Resources.GetBuiltinResource<Mesh>(name);
+        var script = (SwitchObstacleComponent)target;
+        foreach(Transform trans in script.transform)
+        {
+            Undo.DestroyObjectImmediate(trans.gameObject);
+        }
+        var temporary = Instantiate(obstaclePrefabs[prefabIndex], script.transform);
+        Undo.RecordObject(temporary, "Added object");
     }
 
     private static string[] GetAllFilePathsAtFolder(string filter, string folderPath)
