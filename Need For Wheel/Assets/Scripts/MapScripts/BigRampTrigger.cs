c5fce92db49df79e5cc69c4558ddaababb0c22b0
WashFall <carl.christensen98@gmail.com>
2022-05-03 10:11:39 +0200

    Reworked some collision code, and switched colliders

--- Need For Wheel/Assets/Scripts/MapScripts/BigRampTrigger.cs
+++ Need For Wheel/Assets/Scripts/MapScripts/BigRampTrigger.cs
@@ -6,8 +6,8 @@ public class BigRampTrigger : MonoBehaviour
 {
     private void OnTriggerEnter(Collider collision)
     {
-        collision.gameObject.GetComponent<InputManager>().flying = true;
-        PlayerController.State = PlayerState.Flying;
-        //collision.gameObject.GetComponent<PlayerController>().controls = collision.gameObject.GetComponent<FlyingControls>();
+        if(collision.tag == "Player")
+            collision.gameObject.GetComponent<InputManager>().flying = true;
+            PlayerController.State = PlayerState.Flying;
     }
 }
