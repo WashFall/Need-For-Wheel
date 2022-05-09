91bdd0dadd9c82ec07b3071197bf210ecb9420c7
WashFall <carl.christensen98@gmail.com>
2022-05-09 10:06:08 +0200

    Made bricks despawn

--- Need For Wheel/Assets/Scripts/MapScripts/BrickCollision.cs
+++ Need For Wheel/Assets/Scripts/MapScripts/BrickCollision.cs
@@ -4,9 +4,36 @@ using UnityEngine;
 
 public class BrickCollision : MonoBehaviour
 {
+    public GameObject player;
+
+    private Vector3 pos;
+    private bool hasCrashed = false;
+    private GameObject crashObject;
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Player")
+        {
             GetComponent<Rigidbody>().isKinematic = false;
+            crashObject = other.gameObject;
+            hasCrashed = true;
+        }
+    }
+
+    private void Start()
+    {
+        pos = transform.position;
+    }
+
+    private void Update()
+    {
+        if (hasCrashed)
+        {
+            if(pos.z < crashObject.transform.position.z - 20)
+            {
+                Debug.Log("jello");
+                Destroy(gameObject);
+            }
+        }
     }
 }
